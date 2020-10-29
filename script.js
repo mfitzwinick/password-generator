19589f7b59283a476eaf657e11d41e3a392f865b
Michele Fitzgerald-Winick <mfitzwinick@gmail.com>
2020-10-29 15:32:29 -0700

    putting in if statements and for loop

--- script.js
+++ script.js
@@ -37,32 +37,42 @@ var inputUpper = confirm("Would you like upper case letters in your password?")
 var inputSpecial = confirm("Would you like special letters in your password?")
 var inputNumber = confirm("Would you like numbers in your password?")
 var inputAmount = prompt ("How many characters would you like to include in your password (must be between 8 and 128")
+var generateBtn = document.querySelector("#generate");
+var allInputs = [];
+//AllInputs.push(EmployeeInputs)
 
 // Create a for loop for the number of times there is a character choice
-var allInputs = [];
-var EmployeeInputs = //ALL ABOVE--HOW????//
-AllInputs.push(EmployeeInputs)
+// 
+// var EmployeeInputs = //ALL ABOVE--HOW????//
+// 
+console.log(inputAmount)
 
-
-
-
-
-
-
-
-//if inputLower === true {
-    //put into new array--review...
-
-//}
-
-
-
-
+if (inputLower ===true) {
+  allInputs.push(lowercase)
+}
+if (inputUpper ===true) {
+  allInputs.push(uppercase)
+}
+if (inputSpecial === true) {
+  allInputs.push(specialChar)
+}
+if (inputNumber === true) {
+  allInputs.push(numeric)
+}
+console.log(allInputs[Math.floor(Math.random() * allInputs.length)])
+function generatePassword() {
+  var passwordText = []; 
+  for (var i = 0; i < inputAmount; i++) {
+   //var randomSelection = Math.floor(Math.random() * allInputs.length)
+   passwordText.push(allInputs[Math.floor(Math.random() * allInputs.length)])
+  }
+  return passwordText.toString() //usejoin
+}
 
 
 
 // Assignment Code---DOES THIS CORRESPOND WITH THE BUTTON???
-var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
